c916c1f08ddc0814864bfc7ca991e5e717100ed1
Kaf <coffeechopdev@gmail.com>
2026-03-09 22:46:53 -0300

    Align 1.21.11 Forge with template

--- 1.21.11/forge/src/main/java/com/iamkaf/liteminer/forge/LiteminerForge.java
+++ 1.21.11/forge/src/main/java/com/iamkaf/liteminer/forge/LiteminerForge.java
@@ -1,13 +1,24 @@
 package com.iamkaf.liteminer.forge;
 
 import com.iamkaf.liteminer.Constants;
+import com.iamkaf.liteminer.Liteminer;
+import com.iamkaf.liteminer.LiteminerClient;
 import com.iamkaf.liteminer.LiteminerMod;
+import fuzs.forgeconfigapiport.forge.api.v5.NeoForgeConfigRegistry;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.loading.FMLEnvironment;
 
 @Mod(Constants.MOD_ID)
 public class LiteminerForge {
 
     public LiteminerForge() {
+        NeoForgeConfigRegistry.INSTANCE.register(Constants.MOD_ID, ModConfig.Type.COMMON, Liteminer.CONFIG_SPEC);
+        if (FMLEnvironment.dist == Dist.CLIENT) {
+            NeoForgeConfigRegistry.INSTANCE.register(Constants.MOD_ID, ModConfig.Type.CLIENT, LiteminerClient.CONFIG_SPEC);
+            LiteminerClient.init();
+        }
         LiteminerMod.init();
     }
 }
--- 1.21.11/forge/src/main/java/com/iamkaf/liteminer/platform/ForgePlatformHelper.java
+++ 1.21.11/forge/src/main/java/com/iamkaf/liteminer/platform/ForgePlatformHelper.java
@@ -26,4 +26,25 @@ public class ForgePlatformHelper implements IPlatformHelper {
     public java.nio.file.Path getConfigDirectory() {
         return FMLPaths.CONFIGDIR.get();
     }
+
+    @Override
+    public int getBlockExperience(
+        net.minecraft.server.level.ServerLevel level,
+        net.minecraft.core.BlockPos pos,
+        net.minecraft.world.level.block.state.BlockState state,
+        net.minecraft.world.level.block.entity.BlockEntity blockEntity,
+        net.minecraft.world.entity.Entity breaker,
+        net.minecraft.world.item.ItemStack tool
+    ) {
+        var enchantments = level.registryAccess().lookupOrThrow(net.minecraft.core.registries.Registries.ENCHANTMENT);
+        int fortuneLevel = net.minecraft.world.item.enchantment.EnchantmentHelper.getItemEnchantmentLevel(
+            enchantments.getOrThrow(net.minecraft.world.item.enchantment.Enchantments.FORTUNE),
+            tool
+        );
+        int silkTouchLevel = net.minecraft.world.item.enchantment.EnchantmentHelper.getItemEnchantmentLevel(
+            enchantments.getOrThrow(net.minecraft.world.item.enchantment.Enchantments.SILK_TOUCH),
+            tool
+        );
+        return state.getExpDrop(level, level.random, pos, fortuneLevel, silkTouchLevel);
+    }
 }
